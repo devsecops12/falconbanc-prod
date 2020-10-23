4220aab8b3dfa92be8caa15ed95f394f0819f3f4
anthony12 <38588965+devsecops12@users.noreply.github.com>
2020-10-23 16:04:22 -0400

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 resource "ibm_is_vpc" "iac_iks_vpc" {
-  name = "${var.project_name}-${var.environment}-vpc"
+  name = "${var.project_name}-${var.environment}-commonservices"
   resource_group = data.ibm_resource_group.group.id
   address_prefix_management = "manual"
 }
