2be6868c2d06995e6b534175a5dea5366cd0b817
anthony12 <38588965+devsecops12@users.noreply.github.com>
2020-10-26 11:52:48 -0400

    Update network.tf

--- network.tf
+++ network.tf
@@ -37,8 +37,6 @@ resource "ibm_is_security_group_rule" "iac_iks_security_group_rule_tcp_k8s" {
   }
 }
 
- }
-
 resource "ibm_tg_gateway" "new_tg_gw"{
   name      = "front-office-tg"
   location  = "eu-de"
